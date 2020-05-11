0a176a2de02c3e3a7e11b05a15cd766eb44364f3
Stefan <stefan.kandic@yahoo.com>
2020-05-11 06:29:06 +0200

    create prototype of app

---
+++ front/src/components/SideBar.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-container class="top-cont px-6">
+    <v-row>
+      <v-col class="pl-8">
+        <div class="display-1"> New painter</div>
+      </v-col>
+    </v-row>
+
+    <v-form>
+    <v-row class="mt-4">
+      <v-col cols="8" class="pa-0">
+        <v-text-field
+          label="Name"
+          prepend-icon="mdi-map"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-6">
+      <v-col cols="8" class="pa-0">
+        <v-text-field
+          label="Place of birth"
+          prepend-icon="mdi-map"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-6">
+      <v-col cols="8" class="pa-0">
+        <v-text-field
+          label="Nationality"
+          prepend-icon="mdi-map"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-6">
+      <v-col cols="8" class="pa-0">
+        <v-text-field
+          label="Nationality"
+          prepend-icon="mdi-map"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-6">
+      <v-col cols="8" class="pa-0">
+        <v-text-field
+          label="Nationality"
+          prepend-icon="mdi-map"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-6">
+      <v-col cols="8" class="pa-0">
+        <v-select
+            :items="artMovements"
+            menu-props="auto"
+            label="Art movement"
+            hide-details
+            prepend-icon="mdi-map"
+            single-line
+          ></v-select>
+      </v-col>
+    </v-row>
+
+    </v-form>
+    <v-row class="mt-12">
+      <v-col cols="6" class="pa-0" align="center" justify="begin">
+        <v-btn class="blue white--text">Submit</v-btn>
+      </v-col>
+    </v-row>
+
+
+  </v-container>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      artMovements : [
+        'Classicism', 'Post-Impressionism', 'Impressionism',
+        'Cubism', 'Romanticism', 'Gothic', 'Baroque', 'Modern', 
+        'Renaissance', 'Humanism'
+      ]
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+.top-cont{
+}
+
+
+</style>
---
+++ front/src/views/Home.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-container class=" mainContainer">
+
+      <v-row class="pa-5">
+          <div >
+          <v-tabs v-model="tab" class="mb-6" background-color="transparent">
+            <v-tab key="painters">Painters</v-tab>
+            <v-tab key="artwork">Artwork</v-tab>
+            <v-tab key="tags">Tags</v-tab>
+          </v-tabs>
+          </div>
+
+          <v-spacer></v-spacer>
+          <v-btn>DUGME</v-btn>
+      </v-row>
+      <v-row class="mainRow">
+        <v-col cols="4" class="ma-0 py-0 pr-0">
+
+
+          <v-tabs-items v-model="tab">
+            <v-tab-item key="painters" class=""> 
+              <SideBar class="leftSideBar"/>
+            </v-tab-item>
+            <v-tab-item key="artwork" class="">
+              <SideBar class="leftSideBar"/>
+            </v-tab-item>
+            <v-tab-item key="tags" class="">
+              <SideBar class="leftSideBar"/>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-col>
+        
+        <v-col cols="8 blue">
+          <!-- OVDE CE ICI MAPA -->
+          <!-- <NavigationDrawer/> -->
+          <div class="blue"></div>
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+// import NavigationDrawer from "@/components/NavigationDrawer";
+import SideBar from "@/components/SideBar"
+
+export default {
+  name: 'Home',
+  components: {
+    SideBar
+    // NavigationDrawer
+  },
+
+  data() {
+    return {
+      tab : null,
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+.leftSideBar {
+  height: 100%;
+}
+
+.mainContainer {
+  height: 100%;
+  max-width: 5000px;
+}
+
+.mainRow {
+  height: 89%;
+}
+
+
+</style>
