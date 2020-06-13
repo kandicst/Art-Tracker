4e515c592a6294dfd139c7b4ce060538ba55e3a2
Stefan <stefan.kandic@yahoo.com>
2020-06-13 07:06:32 +0200

    add help page

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -134,6 +134,10 @@
     </v-dialog>
 
     <DeleteDialog />
+
+    <router-link to="/help" id="help-hyperlink" target="_blank" :hidden="true">
+      Link Text
+    </router-link>
   </v-container>
 </template>
 
@@ -146,8 +150,8 @@ import FilterComponent from './../components/global/V2Filter';
 import HelpDialog from './../components/global/Dialogs/HelpDialog';
 import ShortcutsDialog from './../components/global/Dialogs/ShortcutsDialog';
 import { mapGetters, mapActions, mapMutations } from 'vuex';
-import DeleteDialog from '../components/global/Dialogs/DeleteDialog'
-import {bus} from '@/main';
+import DeleteDialog from '../components/global/Dialogs/DeleteDialog';
+import { bus } from '@/main';
 
 export default {
   name: 'Home',
@@ -238,6 +242,14 @@ export default {
         });
       }
     },
+
+    openHelp() {
+      document.getElementById('help-hyperlink').click();
+      // let routeData = this.$router.resolve({
+      //   name: '/about',
+      // });
+      // window.open(routeData.href, '_blank');
+    },
   },
 
   computed: {
@@ -255,6 +267,7 @@ export default {
         'ctrl+alt+s': this.showShorcut,
         'ctrl+alt+h': this.showHelp,
         'ctrl+alt+n': this.openDialog,
+        'ctrl+f1': this.openHelp,
       };
     },
   },
